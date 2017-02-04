e113e2d83dc0d26958b6b8ba016cc53bdd9a21a1
mortenjust <morten.just@genstart.dk>
2017-02-04 08:35:52 -0800

    add silent option for reset model

--- resetmodel.sh
+++ resetmodel.sh
@@ -1,7 +1,13 @@
 [ -z "$SMALLIMAGES" ] && . $(dirname $0)/_vars.sh
 
-read -p "This will delete all caches. Are you sure? y/n" -n 1 -r
-echo    # (optional) move to a new line
+
+if [ "$1" == "silently" ] ; then
+    REPLY="Y"
+else
+    read -p "This will delete all caches. Are you sure? y/n" -n 1 -r
+    echo    # (optional) move to a new line
+fi
+
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     # delete bottlenecks 
