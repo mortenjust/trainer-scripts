05efd50ec73c74f5326aacb268368914bab03531
mortenjust <morten.just@genstart.dk>
2017-02-03 16:36:02 -0800

    move videos to root

--- prepareandtrain.sh
+++ prepareandtrain.sh
@@ -1,7 +1,7 @@
 source $(dirname $0)/_vars.sh
 
 # extract all videos
-extractframes.sh $TFBASE/images/videos/*
+extractframes.sh $TFBASE/videos/*
 
 # resize all images
 find ~/tf_files/images/originals -type f \( -name "*.jpg" -or -name "*.JPG" \) | xargs resizeimage.sh # -f
